a2fc47de3c4c9c99d1faffa555f7ece30125dbb5
Honey Jill Hubahib <jillhubahib@gmail.com>
2019-09-25 10:09:16 +0800

    Separate shared example in a different file so other repo can use it

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,12 +7,11 @@ require "database_cleaner"
 require "rails/all"
 require "rspec/rails"
 require "shoulda-matchers"
+require "rspec_examples"
 
 if ENV["COVERAGE"]
   require "simplecov"
-  SimpleCov.start do
-    coverage_dir("tmp/coverage")
-   end
+  SimpleCov.start { coverage_dir("tmp/coverage") }
 else
   puts "* testhq/coverage was required, but not configured using valid environment settings. No coverage reports will be generated. See https://github.com/OneHQ/testhq#code-coverage."
 end
@@ -22,8 +21,6 @@ Combustion.initialize! :all do
 end
 
 Combustion::Application.load_tasks
-
-Dir[Rails.root.join("../support/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
@@ -46,23 +43,4 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-
-  config.extend Module.new {
-    def has_validated_attribute(type, attr, *args, &block)
-      it_behaves_like "#{ type.gsub("_", " ") } attribute", attr, *args, &block
-    end
-
-    # Provide dynamic methods wrappers to shared behaviors.
-    #
-    #=== Examples
-    #  has_validated_name_field(:first_name)      # Same as `it_behaves_like "name attribute", :first_name`
-    #  has_validated_zip_code_field(:first_name)  # Same as `it_behaves_like "zip code field", :first_name`
-    def method_missing(name, *args, &block)
-      if /\Ahas_validated_(?<type>\w*)_attribute\Z/ =~ name
-        has_validated_attribute(type, *args, &block)
-      else
-        super
-      end
-    end
-  }
 end
